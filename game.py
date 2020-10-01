13b9d961dd692b2b1e9f9b4cb448ee900991028b
Константин <olenov1994@gmail.com>
2020-10-01 21:30:33 +0900

    karo4e
    Signed-off-by: Константин <olenov1994@gmail.com>

--- game.py
+++ game.py
@@ -6,13 +6,13 @@ pygame.display.set_caption("FLAME-GAME")
 player = pygame.image.load("idle.png")
 bg = pygame.image.load("bg.png")
 
-x = 50
-y = 50
+x = 0
+y = 0
 speed = 5
 
 clock = pygame.time.Clock()
-
 run = True
+win.blit(bg, (0, 0))
 while(run):
     clock.tick(30)
     for event in pygame.event.get():
@@ -21,17 +21,28 @@ while(run):
 
     keys = pygame.key.get_pressed()
 
+    rel_x = x % bg.get_rect().width
+
     if keys[pygame.K_LEFT]:
-        bg.scroll(5, 0)
+        x += 5
+        win.blit(bg, (rel_x-bg.get_rect().width, 0))
+
+        if rel_x < 1000:
+            win.blit(bg,(rel_x, 0))
     elif keys [pygame.K_RIGHT]:
-        bg.scroll(-5, 0)
+        x -= 5
+        win.blit(bg, (rel_x-bg.get_rect().width, 0))
+
+        if rel_x < 1000:
+            win.blit(bg, (rel_x, 0))
+
     '''elif keys [pygame.K_UP]:
         y -= speed
     elif keys [pygame.K_DOWN]:
         y += speed'''
 
 
-    win.blit(bg, (0, 0))
+
     win.blit(player, (50,50))
     pygame.display.update()
 
